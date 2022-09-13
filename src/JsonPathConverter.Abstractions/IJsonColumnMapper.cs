c702d46a67c9544bfdd3efaa9d4022cec8215af0
azir <zlwu@in-road.com>
2022-09-13 17:17:57 +0800

    feat: add capture object

--- src/JsonPathConverter.Abstractions/IJsonColumnMapper.cs
+++ src/JsonPathConverter.Abstractions/IJsonColumnMapper.cs
@@ -13,5 +13,7 @@ namespace JsonPathConverter.Abstractions
         JsonMapResult<IEnumerable<IDictionary<string, object?>>> MapToCollection(string jsonSourceStr, JsonPathRoot jsonPathRoot);
 
         JsonMapResult<IDictionary<string, object?>> MapToDic(string jsonSourceStr, JsonPathRoot jsonPathRoot);
+
+        TData? CaptureObject<TData>(string jsonSourceStr, string path);
     }
 }
